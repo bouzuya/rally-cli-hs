4f473ca7a8b1b36add21355833d2be3f3f474732
bouzuya <m@bouzuya.net>
2017-11-04 15:47:04 +0900

    add getCredential

--- src/Command/Export.hs
+++ src/Command/Export.hs
@@ -1,6 +1,22 @@
 module Command.Export (export') where
 
-import           Prelude (IO, undefined)
+import           Data.Maybe         (Maybe)
+import           Prelude            (IO, Show, String, print, pure, return, ($),
+                                     (<$>), (<*>))
+import           System.Environment (lookupEnv)
+
+type Email = String
+type Password = String
+data Credential = Credential Email Password deriving (Show)
+
+getCredential :: IO (Maybe Credential)
+getCredential = do
+  email <- lookupEnv "RALLY_EMAIL"
+  password <- lookupEnv "RALLY_PASSWORD"
+  pure $ Credential <$> email <*> password
 
 export' :: IO ()
-export' = undefined
+export' = do
+  credential <- getCredential
+  print credential
+  return ()
