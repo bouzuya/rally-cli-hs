9918ce3a9614701b3b409de0242faec30ee46779
bouzuya <m@bouzuya.net>
2017-11-04 20:12:58 +0900

    add getStampRally

--- src/Command/Export.hs
+++ src/Command/Export.hs
@@ -1,21 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Command.Export (export') where
 
-import           Data.Aeson          (FromJSON (parseJSON), ToJSON (toJSON),
-                                      decode, object, withObject, (.:), (.=))
-import           Data.Maybe          (Maybe (Just, Nothing))
-import           Network.HTTP.Simple (Request, getResponseBody, httpLBS,
-                                      parseRequest, setRequestBodyJSON,
-                                      setRequestMethod, setRequestPath,
-                                      setRequestQueryString)
-import           Prelude             (IO, Show, String, print, pure, putStrLn,
-                                      ($), (<$>), (<*>))
-import           System.Environment  (lookupEnv)
-import           System.Exit         (exitFailure)
+import           Data.Aeson           (FromJSON (parseJSON), ToJSON (toJSON),
+                                       decode, object, withObject, (.:), (.=))
+import qualified Data.ByteString.UTF8 as B
+import           Data.Maybe           (Maybe (Just, Nothing))
+import           Data.Monoid          ((<>))
+import           Network.HTTP.Simple  (Request, getResponseBody, httpLBS,
+                                       parseRequest, setRequestBodyJSON,
+                                       setRequestHeaders, setRequestMethod,
+                                       setRequestPath, setRequestQueryString)
+import           Prelude              (IO, Show, String, print, pure, putStrLn,
+                                       ($), (<$>), (<*>))
+import           System.Environment   (lookupEnv)
+import           System.Exit          (exitFailure)
 
 type Email = String
 type Password = String
 data Credential = Credential Email Password deriving (Show)
+type StampRallyId = String
+data StampRally = StampRally StampRallyId deriving (Show)
 data Token = Token String deriving (Show)
 
 getCredential :: IO (Maybe Credential)
@@ -30,20 +34,36 @@ instance ToJSON Credential where
            , "password" .= password
            ]
 
+instance FromJSON StampRally where
+  parseJSON = withObject "StampRally" $ \v -> StampRally <$> v .: "id"
+
 instance FromJSON Token where
   parseJSON = withObject "Token" $ \v -> Token <$> v .: "token"
 
-createToken :: Credential -> Request -> IO (Maybe Token)
-createToken credential baseRequest =
-  sendRequest $ createTokenRequest credential baseRequest :: IO (Maybe Token)
+createToken :: Request -> IO (Maybe Token)
+createToken = sendRequest
 
 createTokenRequest :: Credential -> Request -> Request
 createTokenRequest credential baseRequest =
   setRequestMethod "POST"
     $ setRequestBodyJSON credential
     $ setRequestPath "/tokens"
-    $ setRequestQueryString [("viewtype", Just "admin")]
+    $ setRequestQueryString [("view_type", Just "admin")]
     $ baseRequest
+
+getStampRally :: Request -> IO (Maybe StampRally)
+getStampRally = sendRequest
+
+getStampRallyRequest :: StampRallyId -> Token -> Request -> Request
+getStampRallyRequest stampRallyId (Token token) baseRequest =
+  setRequestMethod "GET"
+    $ setRequestPath path
+    $ setRequestHeaders [("Authorization", authHeader)]
+    $ setRequestQueryString [("view_type", Just "admin")]
+    $ baseRequest
+  where
+    path = B.fromString $ "/stamp_rallies/" <> stampRallyId
+    authHeader = B.fromString $ "Token token=\"" <> token <> "\""
 
 sendRequest :: (FromJSON a) => Request -> IO (Maybe a)
 sendRequest request = do
@@ -52,12 +72,19 @@ sendRequest request = do
 
 export' :: IO ()
 export' = do
+  let stampRallyId = "xxxxxxxxxxxxxxxx"
   baseRequest <- parseRequest "https://api.rallyapp.jp"
   credential <- getCredential
   print credential
   token <- case credential of
-    Just x -> createToken x baseRequest
+    Just x -> createToken $ createTokenRequest x baseRequest
     Nothing -> do
       putStrLn "check RALLY_EMAIL and RALLY_PASSWORD"
       exitFailure
   print token
+  stampRally <- case token of
+    Just x -> getStampRally $ getStampRallyRequest stampRallyId x baseRequest
+    Nothing -> do
+      putStrLn "StampRally"
+      exitFailure
+  print stampRally
