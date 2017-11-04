5aa344d713f4537a234e6d1f92cc61e387734908
bouzuya <m@bouzuya.net>
2017-11-04 18:16:35 +0900

    add createTokenRequest

--- src/Command/Export.hs
+++ src/Command/Export.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Command.Export (export') where
 
-import           Data.Aeson         (ToJSON (toJSON), encode, object, (.=))
-import           Data.Maybe         (Maybe)
-import           Prelude            (IO, Show, String, print, pure, return, ($),
-                                     (<$>), (<*>))
-import           System.Environment (lookupEnv)
+import           Data.Aeson          (ToJSON (toJSON), object, (.=))
+import           Data.Maybe          (Maybe (Just, Nothing))
+import           Network.HTTP.Simple (Request, getResponseBody, httpLBS,
+                                      parseRequest, setRequestBodyJSON,
+                                      setRequestMethod, setRequestPath,
+                                      setRequestQueryString)
+import           Prelude             (IO, Show, String, print, pure, putStrLn,
+                                      ($), (<$>), (<*>))
+import           System.Environment  (lookupEnv)
 
 type Email = String
 type Password = String
@@ -19,12 +23,27 @@ getCredential = do
 
 instance ToJSON Credential where
   toJSON (Credential email password) =
-     object [ "email" .= email
-            , "password" .= password
-            ]
+    object [ "email" .= email
+           , "password" .= password
+           ]
+
+createTokenRequest :: Credential -> Request -> Request
+createTokenRequest credential baseRequest =
+  setRequestMethod "POST"
+    $ setRequestBodyJSON credential
+    $ setRequestPath "/tokens"
+    $ setRequestQueryString [("viewtype", Just "admin")]
+    $ baseRequest
 
 export' :: IO ()
 export' = do
+  baseRequest <- parseRequest "https://api.rallyapp.jp"
   credential <- getCredential
-  print $ encode <$> credential
-  return ()
+  print credential
+  case credential of
+    Just x -> do
+      let request = createTokenRequest x baseRequest
+      json <- httpLBS request
+      print $ getResponseBody json
+    Nothing ->
+      putStrLn "check RALLY_EMAIL and RALLY_PASSWORD"
