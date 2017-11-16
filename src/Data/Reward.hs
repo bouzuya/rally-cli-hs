1842b51f4d4d5b94c041460607ad29ed796d0ccf
bouzuya <m@bouzuya.net>
2017-11-17 08:48:17 +0900

    add Defailt of Reward

--- src/Data/Reward.hs
+++ src/Data/Reward.hs
@@ -1,15 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Data.Reward (Reward, RewardId, RewardList) where
+module Data.Reward ( Reward
+                   , RewardId
+                   , RewardList
+                   , getRewardId
+                   , getRewardSummaryList
+                   ) where
 
-import           Data.Aeson (FromJSON (parseJSON), withObject, (.:))
-import           Prelude    (Int, Show, ($), (<$>))
+import           Data.Aeson  (FromJSON (parseJSON), withObject, (.:))
+import           Data.Detail (Detail)
+import           Prelude     (Int, Show, ($), (<$>), (<*>))
 
 type RewardId = Int
-data Reward = Reward RewardId deriving (Show)
-data RewardList = RewardList [Reward] deriving (Show)
+data Reward = Reward RewardId [Detail] deriving (Show)
+data RewardSummary = RewardSummary RewardId deriving (Show)
+data RewardList = RewardList [RewardSummary] deriving (Show)
+
+getRewardId :: RewardSummary -> RewardId
+getRewardId (RewardSummary rewardId) = rewardId
+
+getRewardSummaryList :: RewardList -> [RewardSummary]
+getRewardSummaryList (RewardList rewardSummaryList) = rewardSummaryList
 
 instance FromJSON Reward where
-  parseJSON = withObject "Reward" $ \v -> Reward <$> v .: "id"
+  parseJSON = withObject "Reward" $ \v -> Reward
+    <$> v .: "id"
+    <*> v .: "details"
+
+instance FromJSON RewardSummary where
+  parseJSON = withObject "RewardSummary" $ \v -> RewardSummary
+    <$> v .: "id"
 
 instance FromJSON RewardList where
-  parseJSON = withObject "RewardList" $ \v -> RewardList <$> v .: "rewards"
+  parseJSON = withObject "RewardList" $ \v -> RewardList
+    <$> v .: "rewards"
+
--- src/Request.hs
+++ src/Request.hs
@@ -14,7 +14,8 @@ import qualified Data.ByteString.UTF8 as B
 import           Data.Credential      (Credential)
 import           Data.Maybe           (Maybe (Just, Nothing))
 import           Data.Monoid          ((<>))
-import           Data.Reward          (RewardList)
+import           Data.Reward          (Reward, RewardId, RewardList,
+                                       getRewardId, getRewardSummaryList)
 import           Data.Spot            (Spot, SpotId, SpotList, getSpotId,
                                        getSpotSummaryList)
 import           Data.StampRally      (StampRally, StampRallyId)
@@ -40,12 +41,47 @@ createTokenRequest credential request =
 defaultRequest :: IO Request
 defaultRequest = parseRequest "https://api.rallyapp.jp"
 
-getRewardList :: StampRallyId -> Token -> Request -> IO (Maybe RewardList)
-getRewardList stampRallyId token request =
-  sendRequest $ getRewardListRequest stampRallyId token request
+getReward :: RewardId -> Token -> Request -> IO (Maybe Reward)
+getReward rewardId token request =
+  sendRequest $ getRewardRequest rewardId token request
 
-getRewardListRequest :: StampRallyId -> Token -> Request -> Request
-getRewardListRequest stampRallyId token request =
+getRewardRequest :: RewardId -> Token -> Request -> Request
+getRewardRequest rewardId token request =
+  setRequestMethod "GET"
+    $ setRequestPath path
+    $ setRequestHeaders [("Authorization", authHeader)]
+    $ setRequestQueryString [("view_type", Just "admin")]
+    $ request
+ where
+  path       = B.fromString $ "/rewards/" <> show rewardId
+  authHeader = B.fromString $ getAuthHeader token
+
+getRewardList :: StampRallyId -> Token -> Request -> IO (Maybe [Reward])
+getRewardList stampRallyId token request = do
+  rewardSummaryList <- getRewardSummaryList' stampRallyId token request
+  case rewardSummaryList of
+    Just x -> do
+      rewards <-
+        foldM
+          ( \a rewardSummary -> do
+            reward <- getReward' rewardSummary
+            pure $ a <> [reward]
+          )
+          []
+        (getRewardSummaryList x) :: IO [Maybe Reward]
+      pure $ traverse id rewards
+    Nothing -> pure Nothing
+ where
+  getReward' rewardSummary =
+    getReward (getRewardId rewardSummary) token request
+
+getRewardSummaryList'
+  :: StampRallyId -> Token -> Request -> IO (Maybe RewardList)
+getRewardSummaryList' stampRallyId token request =
+  sendRequest $ getRewardSummaryListRequest stampRallyId token request
+
+getRewardSummaryListRequest :: StampRallyId -> Token -> Request -> Request
+getRewardSummaryListRequest stampRallyId token request =
   setRequestMethod "GET"
     $ setRequestPath path
     $ setRequestHeaders [("Authorization", authHeader)]
@@ -72,22 +108,22 @@ getSpotRequest spotId token request =
 
 getSpotList :: StampRallyId -> Token -> Request -> IO (Maybe [Spot])
 getSpotList stampRallyId token request = do
-  spotSummaryList <- getSpotSummaryList'  stampRallyId token request
+  spotSummaryList <- getSpotSummaryList' stampRallyId token request
   case spotSummaryList of
-    Just x  -> do
-      spots <- foldM
-        (\a spotSummary -> do
-          spot <- getSpot' spotSummary
-          pure $ a <> [spot])
-        []
-        (getSpotSummaryList x)
-        :: IO [Maybe Spot]
+    Just x -> do
+      spots <-
+        foldM
+          ( \a spotSummary -> do
+            spot <- getSpot' spotSummary
+            pure $ a <> [spot]
+          )
+          []
+          (getSpotSummaryList x) :: IO [Maybe Spot]
       pure $ traverse id spots
     Nothing -> pure Nothing
-  where
-    getSpot' spotSummary = getSpot (getSpotId spotSummary) token request
+  where getSpot' spotSummary = getSpot (getSpotId spotSummary) token request
 
-getSpotSummaryList' ::  StampRallyId -> Token -> Request -> IO (Maybe SpotList)
+getSpotSummaryList' :: StampRallyId -> Token -> Request -> IO (Maybe SpotList)
 getSpotSummaryList' stampRallyId token request =
   sendRequest $ getSpotSummaryListRequest stampRallyId token request
 
