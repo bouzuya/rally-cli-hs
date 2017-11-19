b9c3ced2f7c4f00299b51bc88390216acd4020a2
bouzuya <m@bouzuya.net>
2017-11-19 14:39:50 +0900

    add Image of StampRally

---
+++ src/Data/Image.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Data.Image (Image) where
+
+import           Data.Aeson (FromJSON (parseJSON), ToJSON (toJSON), object,
+                             withObject, (.:), (.=))
+import           Prelude    (Int, Show, String, ($), (<$>), (<*>))
+
+data Image = Image String Int String String deriving (Show)
+
+instance FromJSON Image where
+  parseJSON = withObject "Image" $ \v -> Image
+    <$> v .: "id"
+    <*> v .: "position"
+    <*> v .: "s64"
+    <*> v .: "s640"
+
+instance ToJSON Image where
+  toJSON (Image id position s64 s640) =
+    object [ "id" .= id
+           , "position" .= position
+           , "s64" .= s64
+           , "s640" .= s640
+           ]
--- src/Data/StampRally.hs
+++ src/Data/StampRally.hs
@@ -4,18 +4,21 @@ module Data.StampRally (StampRally, StampRallyId) where
 import           Data.Aeson  (FromJSON (parseJSON), ToJSON (toJSON), object,
                               withObject, (.:), (.=))
 import           Data.Detail (Detail)
+import           Data.Image  (Image)
 import           Prelude     (Show, String, ($), (<$>), (<*>))
 
 type StampRallyId = String
-data StampRally = StampRally StampRallyId [Detail] deriving (Show)
+data StampRally = StampRally StampRallyId [Detail] [Image] deriving (Show)
 
 instance FromJSON StampRally where
   parseJSON = withObject "StampRally" $ \v -> StampRally
     <$> v .: "id"
     <*> v .: "details"
+    <*> v .: "images"
 
 instance ToJSON StampRally where
-  toJSON (StampRally id details) =
+  toJSON (StampRally id details images) =
     object [ "id" .= id
            , "details" .= details
+           , "images" .= images
            ]
